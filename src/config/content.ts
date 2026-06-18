5355f664d3189f9e96945e46a47895e8cd5284d3
Mode-s <shota.umemoto7@gmail.com>
2026-06-18 11:42:35 +0900

    price section

--- src/config/content.ts
+++ src/config/content.ts
@@ -9,11 +9,10 @@ import item01 from '../assets/images/101/101.jpg';
 import item02 from '../assets/images/102/102.jpg';
 import item03 from '../assets/images/201/201.jpg';
 import item04 from '../assets/images/202/202.jpg';
-import item05 from '../assets/images/logo.png';
 
 type Logo = { src: ImageMetadata; alt: string; href: string };
 type NavItem = { label: string; href: string };
-type Item = { image: ImageMetadata; name: string; price?: string; description?: string };
+type Item = { image: ImageMetadata; name: string; price?: string; description?: string; href?: string };
 
 export const content = {
   header: {
@@ -34,10 +33,10 @@ export const content = {
   menu: {
     title: "Rooms",
     items: [
-      { image: item01, name: "101号室", price: "10", description: "あ" },
-      { image: item02, name: "102号室", price: "20", description: "い" },
-      { image: item03, name: "201号室", price: "30", description: "う" },
-      { image: item04, name: "202号室", price: "40", description: "え" },
+      { image: item01, name: "101号室", price: "10", description: "あ", href: "" },
+      { image: item02, name: "102号室", price: "20", description: "い", href: "" },
+      { image: item03, name: "201号室", price: "30", description: "う", href: "" },
+      { image: item04, name: "202号室", price: "40", description: "え", href: "" },
     ] satisfies Item[],
   },
 } as const;
